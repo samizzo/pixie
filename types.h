0277296b21fca867120b6aa3396b38cfdcfb2f18
Sam Izzo <sam@horsedrawngames.com>
2018-09-09 13:33:28 +1000

    Added min/max macros.

--- types.h
+++ types.h
@@ -2,3 +2,11 @@
 
 typedef unsigned int uint32;
 typedef unsigned char uint8;
+
+#ifndef max
+#define max(a,b)            (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef min
+#define min(a,b)            (((a) < (b)) ? (a) : (b))
+#endif
