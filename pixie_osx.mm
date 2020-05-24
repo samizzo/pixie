a3ab764a768b5f45fe01a8b81885c899df34db63
Sam Izzo <sam@horsedrawngames.com>
2020-05-24 21:53:11 +1000

    Added custom key mappings for ASCII keys on osx because they aren't contiguous like scan codes on Windows.

--- pixie_osx.mm
+++ pixie_osx.mm
@@ -139,9 +139,6 @@ static const int FrameBufferBitDepth = 8;
 
 void Window::PlatformInit()
 {
-    for (int i = 0; i < Key_Num; i++)
-        m_keyMap[i] = -1;
-
     m_keyMap[Key_Backspace] = kVK_Delete;
     m_keyMap[Key_Tab] = kVK_Tab;
     m_keyMap[Key_Enter] = kVK_Return;
@@ -171,6 +168,55 @@ void Window::PlatformInit()
     m_keyMap[Key_F10] = kVK_F10;
     m_keyMap[Key_F11] = kVK_F11;
     m_keyMap[Key_F12] = kVK_F12;
+
+    m_keyMap[' '] = kVK_Space;
+    m_keyMap['0'] = kVK_ANSI_0;
+    m_keyMap['1'] = kVK_ANSI_1;
+    m_keyMap['2'] = kVK_ANSI_2;
+    m_keyMap['3'] = kVK_ANSI_3;
+    m_keyMap['4'] = kVK_ANSI_4;
+    m_keyMap['5'] = kVK_ANSI_5;
+    m_keyMap['6'] = kVK_ANSI_6;
+    m_keyMap['7'] = kVK_ANSI_7;
+    m_keyMap['8'] = kVK_ANSI_8;
+    m_keyMap['9'] = kVK_ANSI_9;
+    m_keyMap['['] = kVK_ANSI_LeftBracket;
+    m_keyMap['\\'] = kVK_ANSI_Backslash;
+    m_keyMap[']'] = kVK_ANSI_RightBracket;
+    m_keyMap['`'] = kVK_ANSI_Grave;
+    m_keyMap['\''] = kVK_ANSI_Quote;
+    m_keyMap['='] = kVK_ANSI_Equal;
+    m_keyMap['-'] = kVK_ANSI_Minus;
+    m_keyMap['.'] = kVK_ANSI_Period;
+    m_keyMap['/'] = kVK_ANSI_Slash;
+    m_keyMap[';'] = kVK_ANSI_Semicolon;
+    m_keyMap[','] = kVK_ANSI_Comma;
+    m_keyMap['A'] = kVK_ANSI_A;
+    m_keyMap['B'] = kVK_ANSI_B;
+    m_keyMap['C'] = kVK_ANSI_C;
+    m_keyMap['D'] = kVK_ANSI_D;
+    m_keyMap['E'] = kVK_ANSI_E;
+    m_keyMap['F'] = kVK_ANSI_F;
+    m_keyMap['G'] = kVK_ANSI_G;
+    m_keyMap['H'] = kVK_ANSI_H;
+    m_keyMap['I'] = kVK_ANSI_I;
+    m_keyMap['J'] = kVK_ANSI_J;
+    m_keyMap['K'] = kVK_ANSI_K;
+    m_keyMap['L'] = kVK_ANSI_L;
+    m_keyMap['M'] = kVK_ANSI_M;
+    m_keyMap['N'] = kVK_ANSI_N;
+    m_keyMap['O'] = kVK_ANSI_O;
+    m_keyMap['P'] = kVK_ANSI_P;
+    m_keyMap['Q'] = kVK_ANSI_Q;
+    m_keyMap['R'] = kVK_ANSI_R;
+    m_keyMap['S'] = kVK_ANSI_S;
+    m_keyMap['T'] = kVK_ANSI_T;
+    m_keyMap['U'] = kVK_ANSI_U;
+    m_keyMap['V'] = kVK_ANSI_V;
+    m_keyMap['W'] = kVK_ANSI_W;
+    m_keyMap['X'] = kVK_ANSI_X;
+    m_keyMap['Y'] = kVK_ANSI_Y;
+    m_keyMap['Z'] = kVK_ANSI_Z;
 }
 
 bool Window::PlatformOpen(const char* title, int width, int height)
